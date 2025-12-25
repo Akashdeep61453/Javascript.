7490413a03c42cb032de088a905ff3e7b13ee43b
Akashdeep Singh <akashmansa58@gmail.com>
2025-12-25 17:20:42 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+"use strict"// treat all js code as newer version
+// alert(3+3) we are using nodejs not browser
+
+console.log("Akash"); // maintain the readibility of code
+console.log(3+3)
+let age = 18
+let state;
+console.log(typeof null)// null is object  and undefined is undefined
+console.log(typeof state)
+/*
+number => 2^53
+bigint
+string boolean
+null => standalone value
+undefined
+symbol=>unique*/
+
+
+
---
+++ test.js
@@ -0,0 +1,9 @@
+const accountId=14453
+let accountEmail = "akash@gmail.com"
+var accountPassword = "12345"
+accountCity = "Punjab"
+let myValue;
+
+// accountId = 2000
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword,accountCity,myValue])
