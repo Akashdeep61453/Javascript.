8186e0b61bd0debf2aff98642c60525610538242
Akashdeep Singh <akashmansa58@gmail.com>
2025-12-26 13:26:04 +0530

    Add files via upload

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,13 +1,13 @@
-// Conversion of Data Types
-let score = false
-console.log(typeof score)
-console.log(typeof (score))
-console.log(score)
+// // Conversion of Data Types
+// let score = false
+// console.log(typeof score)
+// console.log(typeof (score))
+// console.log(score)
 
 
-let valueInNum = Number(score)
-console.log(typeof valueInNum)// prints number
-console.log(valueInNum)
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum)// prints number
+// console.log(valueInNum)
 // half string and number converts to number shows NaN (not a number)
 // if score = null, then also after converting prints 0
 // if score is undefined then after coversion  printf NaN
@@ -20,6 +20,6 @@ let isLoggedIn = 1// 1=>true    0=>false
 let inBooleanLogged = Boolean(isLoggedIn)
 console.log(inBooleanLogged)
 
-let Num = 33
-let stringNum = String(Num)
-console.log(typeof stringNum)
+let Num = ""
+let stringNum = Boolean(Num)
+console.log(stringNum)
